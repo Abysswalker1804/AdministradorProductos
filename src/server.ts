e6d42d15d3c48658b1fa81c33bd9eef4368dea41
Tadeo Palma <tadeo.palavi@gmail.com>
2025-06-03 00:39:09 -0600

    Actualización 03/06/2025

---
+++ src/server.ts
@@ -0,0 +1,24 @@
+import express from 'express'
+import colors from 'colors'
+import router from './router'
+import db from './config/db'
+
+//COnectar a base de datos
+async function connectDB(){
+    try{
+        await db.authenticate()
+        db.sync()
+        console.log(colors.magenta.bold('Conexion exitoosa a la DB'))
+    }catch (error){
+        //console.log(error)
+        console.log(colors.bgRed.white('Hubo un problema al conectar con la DB'))
+    }
+}
+connectDB()
+const server = express()
+
+//Leer datos
+server.use(express.json())
+
+server.use('/api/products',router)
+export default server
